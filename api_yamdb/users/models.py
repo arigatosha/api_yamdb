220253981c1a8d0f41333bfade502f60ca58a977
Anton <115580886+antxrest@users.noreply.github.com>
2023-02-16 16:40:58 +0300

    кавычки

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -3,9 +3,9 @@ from django.db import models
 
 
 class User(AbstractUser):
-    ADMIN = "admin"
-    MODERATOR = "moderator"
-    USER = "user"
+    ADMIN = 'admin'
+    MODERATOR = 'moderator'
+    USER = 'user'
 
     ROLE_CHOICES = (
         (ADMIN, 'admin'),
@@ -14,7 +14,7 @@ class User(AbstractUser):
 
     username = models.CharField(max_length=50, blank=True, null=True, unique=True)
     email = models.EmailField(verbose_name='email', unique=True)
-    role = models.CharField("Роли пользователей", default=USER, choices=ROLE_CHOICES)
+    role = models.CharField('Роли пользователей', default=USER, choices=ROLE_CHOICES)
     bio = models.TextField('Биография', blank=True, )
     token = models.CharField(
         blank=True,
