8e27a36d0b3d961eb82beee380dde922bc3cac81
Anton Molchanov <mo4anov.a@gmail.com>
2023-02-17 15:55:33 +0300

    add v1 API

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,14 +1,8 @@
 from django.urls import include, path
-from rest_framework.routers import DefaultRouter
 
-from .views import (CategoryViewSet, GenreViewSet,
-                    TitleViewSet)
+app_name = 'api'
 
-router = DefaultRouter()
-router.register(r'categories', CategoryViewSet)
-router.register(r'genres', GenreViewSet)
-router.register(r'titles', TitleViewSet)
 
 urlpatterns = [
-    path('v1/', include(router.urls)),
-    ]
+    path('v1/', include('api.v1.urls'))
+]
