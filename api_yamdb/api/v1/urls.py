1d1bceb6cfc7fe5640da329c43ea7662e21ac125
Anton Molchanov <mo4anov.a@gmail.com>
2023-02-22 16:26:00 +0300

    pep8 api views

--- api_yamdb/api/v1/urls.py
+++ api_yamdb/api/v1/urls.py
@@ -1,10 +1,9 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import (
-    CategoryViewSet, CommentViewSet, GenreViewSet,
-    ReviewViewSet, TitleViewSet, Token, UserViewSet,)
-from .views import create_user
+from .views import (CategoryViewSet, CommentViewSet, GenreViewSet,
+                    ReviewViewSet, TitleViewSet, UserViewSet)
+from .views import create_user, Token
 
 router = DefaultRouter()
 
--- api_yamdb/api/v1/views.py
+++ api_yamdb/api/v1/views.py
@@ -1,24 +1,23 @@
-from django.db.models import Avg
 from django.contrib.auth import get_user_model
 from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import send_mail
-from django_filters.rest_framework import DjangoFilterBackend
+from django.db.models import Avg
 from django.shortcuts import get_object_or_404
-from rest_framework import filters, viewsets, status
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters, status, viewsets
 from rest_framework.decorators import action, api_view, permission_classes
-from rest_framework.response import Response
 from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import AccessToken
+from reviews.models import Category, Genre, Review, Title
 
 from .filters import TitleFieldFilter
 from .mixins import CreateListDestroyViewSet
 from .permissions import IsAdministrator, IsAdminOrReadOnly, OwnerOrReadOnly
-from .serializers import (
-    CategorySerializer, CommentSerializer, GenreSerializer,
-    TitleSerializer, ReviewSerializer, OnlyReadTitleSerializer,
-    UserSerializer, RegisterSerializer, MyTokenObtainPairSerializer)
-from reviews.models import Category, Genre, Title, Review
-
+from .serializers import (CategorySerializer, CommentSerializer,
+                          GenreSerializer, MyTokenObtainPairSerializer,
+                          OnlyReadTitleSerializer, RegisterSerializer,
+                          ReviewSerializer, TitleSerializer, UserSerializer)
 
 User = get_user_model()
 
