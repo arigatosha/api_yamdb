a8393299384956b5130f768aaf920c36fa95ef7f
NakiriEri <50824033+NakiriEri@users.noreply.github.com>
2023-02-22 14:32:40 +0300

    Update urls.py

--- api_yamdb/api/v1/urls.py
+++ api_yamdb/api/v1/urls.py
@@ -1,7 +1,9 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-from .views import (CategoryViewSet, CommentViewSet, create_user, GenreViewSet,
-                    ReviewViewSet, TitleViewSet, Token, UserViewSet)
+
+from .views import (
+    CategoryViewSet, CommentViewSet, create_user, GenreViewSet,
+    ReviewViewSet, TitleViewSet,Token, UserViewSet)
 
 
 router = DefaultRouter()
